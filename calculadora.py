61508c3f9a14108e44019e3f7ef54d962ab20a4d
Jorge Santiago Guerrero Osuna <jorge.guerrero.osuna@uabc.edu.mx>
2026-02-28 20:31:14 -0800

    Cambios pequeños
    comentarios extra

--- calculadora.py
+++ calculadora.py
@@ -1,3 +1,4 @@
 class Calculadora:
+    # Método para sumar dos números
     def add(self, a, b):
         return a + b
--- test_calculadora.py
+++ test_calculadora.py
@@ -1,4 +1,5 @@
 from calculadora import Calculadora
+# Test para la función add de la clase Calculadora
 def test_add():
     calc = Calculadora()
     assert calc.add(2, 3) == 5
